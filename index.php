eb345caeb43c37b33c085fbba95328e24935e72d
Manuel Velasquez <wanuelinggmail.com>
2012-03-08 10:07:01 -0330

    tarea 3

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 <html>
 	<head>
 		<title>Video Store</title>
+                <script type="text/javascript" src="JQuery 1.7.1.jsp"></script>
 	</head>
 	<body>
             <div align="center">
             <h1>Video Store</h1>
 	</body>	
-        <a href="Video Store" title="Diveo Store"><img src="Imagenes/Tintin_Shop.jpg" alt="descripcion" /></a>
+        <a title="Diveo Store"><img src="Imagenes/indeximage.jpg" alt="descripcion" /></a>
+        <table>
+            <tr>
+                <td> <textarea cols=”20″ rows=”5″ wrap=”hard” readonly=”yes”></textarea></td>
+                <td><button name ="come">Bienvenido</button></td>
+            </tr>
+            
+        </table>
         </div>
+            
 </html>
