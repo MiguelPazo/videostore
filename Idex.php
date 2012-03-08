ad3e2ded9161bd5350484ea5566e90e45c478c3b
Manuel Velasquez <wanuelinggmail.com>
2012-03-07 22:36:18 -0330

    tarea 01

---
+++ Idex.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Video Store</title>
+	</head>
+	<body>
+            <div align="center">
+            <h1>Video Store</h1>
+	</body>	
+        <a href="Video Store" title="Diveo Store"><img src="Imagenes/Tintin_Shop.jpg" alt="descripcion" /></a>
+        </div>
+</html>
