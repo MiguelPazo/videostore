eeba3ad00074522e25b57e117ed42e590a100c75
Miguel Rodrigo Pazo Sanchez <miguel.ps19@gmail.com>
2012-05-06 23:23:12 -0500

    Plantilla 100%

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-  $("a.new_window").attr("target", "_blank");
-  
-  $("#menuTabs li a").click(function(){
-      $(this).parent('li').find('a').removeClass('active');
-      $(this).addClass('active');
-  });
- });
+    
+});
